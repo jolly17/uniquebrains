c599f74a84cd17438034db28d0e40703b519d755
Goyal <goyalanj@amazon.co.uk>
2025-12-10 12:56:32 +0000

    fix: Remove Coming Soon page, enable marketplace for testing

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import { AuthProvider, useAuth } from './context/AuthContext'
 import Layout from './components/Layout'
 import LandingPage from './pages/LandingPage'
-import ComingSoon from './pages/ComingSoon'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Marketplace from './pages/Marketplace'
@@ -44,11 +43,8 @@ function App() {
           
           <Route path="/" element={<Layout />}>
             <Route index element={<LandingPage />} />
-            <Route path="marketplace" element={<ComingSoon />} />
+            <Route path="marketplace" element={<Marketplace />} />
             <Route path="courses/:courseId" element={<CourseDetail />} />
-            
-            {/* Hidden route for development - access full marketplace */}
-            <Route path="marketplace-dev" element={<Marketplace />} />
             
             <Route path="my-courses" element={
               <ProtectedRoute>
